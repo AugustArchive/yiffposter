d0472c9e3349bd238447eb092fe36340bf154662
Noel <august@augu.dev>
2020-12-25 13:43:52 -0700

    Rewrite to TypeScript

--- update.sh
+++ update.sh
@@ -2,7 +2,6 @@
 
 DOCKER_PID=$(echo $(docker ps -aqf name="autoyiffer") | sed 's/\=//')
 
-python3 -m pip install -U -r requirements.txt
 docker stop $DOCKER_PID
 docker rm $DOCKER_PID
 docker build . --no-cache -t autoyiffer:latest
