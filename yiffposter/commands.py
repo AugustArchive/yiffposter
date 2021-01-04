2761c84600198f71a9e50a21a9e7bce56403c08e
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2021-01-03 20:19:12 -0800

    Fix stupid fucking indentation
    Fuck you python

--- yiffposter/commands.py
+++ yiffposter/commands.py
@@ -18,31 +18,30 @@ import random
 vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
 
 def last_replace(s, old, new):
-    li = s.rsplit(old, 1)
-    return new.join(li)
+  li = s.rsplit(old, 1)
+  return new.join(li)
 
 def make_owo(text):
-    smileys = [';;w;;', '^w^', '>w<', 'UwU', '(・`ω\´・)', '(´・ω・\`)']
+  smileys = [';;w;;', '^w^', '>w<', 'UwU', '(・`ω\´・)', '(´・ω・\`)']
 
-    text = text.replace('L', 'W').replace('l', 'w')
-    text = text.replace('R', 'W').replace('r', 'w')
+  text = text.replace('L', 'W').replace('l', 'w')
+  text = text.replace('R', 'W').replace('r', 'w')
 
-    text = last_replace(text, '!', '! {}'.format(random.choice(smileys)))
-    text = last_replace(text, '?', '? owo')
-    text = last_replace(text, '.', '. {}'.format(random.choice(smileys)))
+  text = last_replace(text, '!', '! {}'.format(random.choice(smileys)))
+  text = last_replace(text, '?', '? owo')
+  text = last_replace(text, '.', '. {}'.format(random.choice(smileys)))
 
-    for v in vowels:
-        if 'n{}'.format(v) in text:
-            text = text.replace('n{}'.format(v), 'ny{}'.format(v))
-        if 'N{}'.format(v) in text:
-            text = text.replace('N{}'.format(v), 'N{}{}'.format('Y' if v.isupper() else 'y', v))
-
-    return text
+  for v in vowels:
+    if 'n{}'.format(v) in text:
+      text = text.replace('n{}'.format(v), 'ny{}'.format(v))
+      if 'N{}'.format(v) in text:
+        text = text.replace('N{}'.format(v), 'N{}{}'.format('Y' if v.isupper() else 'y', v))
+  return text
   
 # Commands
 
 def start(update: Update, ctx: CallbackContext):
-update.message.reply_text("""
+  update.message.reply_text("""
   henlo, I am an autoposter bot that posts yiff every 30 minutes!
   -=- Managed by @auguwu -=-
 
